5c0eb353374f010d54fe51b78e97e928f8c31a7e
Jhonatas Willian <jhonatas.willian.dev@gmail.com>
2026-02-05 14:14:03 -0300

    Initial Commit

---
+++ FocusBear/FocusBearApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusBearApp.swift
+//  FocusBear
+//
+//  Created by Jhonatas Willian on 05/02/26.
+//
+
+import SwiftUI
+
+@main
+struct FocusBearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
